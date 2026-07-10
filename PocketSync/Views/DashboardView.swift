e65e78556bc7176678bb2a31410a360c4fcdac12
bibekupreti <bibekupreti.dev@gmail.com>
2026-07-10 18:55:20 +0545

    Added model and started with the dashboard view

---
+++ PocketSync/Views/DashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardView.swift
+//  PocketSync
+//
+//  Created by Bibek upreti on 10/07/2026.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardView()
+}
